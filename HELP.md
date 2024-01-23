385a4d865e5bbd489d577e767dd5dc203d7ff49b
irina.petrovskaya <irina.petrovskaya@jetbrains.com>
2024-01-23 18:03:31 +0100

    comments

---
+++ HELP.md
@@ -0,0 +1,6 @@
+# Spring Core: basic navigation for supertype
+
+Spring: NavigateToAutowiredCandidates for interface 
+should consider the implementation components injection
+
+https://youtrack.jetbrains.com/issue/IDEA-343849
--- src/main/java/com/mytests/spring/springautowiredimplementationsnavigation/FooBarService.java
+++ src/main/java/com/mytests/spring/springautowiredimplementationsnavigation/FooBarService.java
@@ -1,6 +1,7 @@
 package com.mytests.spring.springautowiredimplementationsnavigation;
 
- 
+// NavigateToAutowiredCandidates suggests only usages in `InjectInterfaces`;
+// the implementations injection (in `InjectImplementations`) are ignored
 public interface FooBarService {
     String getId();
 }
