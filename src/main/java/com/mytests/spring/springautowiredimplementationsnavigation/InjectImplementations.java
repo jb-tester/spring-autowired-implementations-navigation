4c112cb7b10d8a2e1cdc17324c56a1b5372f697b
irina.petrovskaya <irina.petrovskaya@jetbrains.com>
2024-01-31 14:52:23 +0100

    qexplicit qualifiers added

--- src/main/java/com/mytests/spring/springautowiredimplementationsnavigation/InjectImplementations.java
+++ src/main/java/com/mytests/spring/springautowiredimplementationsnavigation/InjectImplementations.java
@@ -1,6 +1,7 @@
 package com.mytests.spring.springautowiredimplementationsnavigation;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 
@@ -10,11 +11,21 @@ public class InjectImplementations {
     @Autowired
     private FooService fooService;
 
+    @Autowired @Qualifier("fooService")
+    private FooService fooServiceQualified;
+
     @Autowired
     private BarService barService;
 
+    @Autowired @Qualifier("barService")
+    private BarService barServiceQualified;
+
     @Autowired
     private SingleImpl singleImpl;
+
+    @Autowired @Qualifier("singleImpl")
+    private SingleImpl singleImplQualified;
+
 
 
 }
--- src/main/java/com/mytests/spring/springautowiredimplementationsnavigation/InjectInterfaces.java
+++ src/main/java/com/mytests/spring/springautowiredimplementationsnavigation/InjectInterfaces.java
@@ -1,6 +1,7 @@
 package com.mytests.spring.springautowiredimplementationsnavigation;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 
@@ -15,6 +16,15 @@ public class InjectInterfaces {
 
     @Autowired
     private SingleImplementationService singleImpl;
+
+    @Autowired @Qualifier("fooService")
+    private FooBarService fooServiceQualified;
+
+    @Autowired @Qualifier("barService")
+    private FooBarService barServiceQualified;
+
+    @Autowired @Qualifier("singleImpl")
+    private SingleImplementationService singleImplQualified;
 
 
 }
